ad17b6b46d53c035662b028f45af5d48bc0a4d2c
JeongChanWoo <ftfgz5093@gmail.com>
2022-10-03 21:08:34 +0900

    책 서비스 목록보기 완료

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -8,8 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 // 클 -> DS -> C -> S -> R -> PS -> DB
 public class BookService {
 
@@ -17,12 +21,17 @@ public class BookService {
 
     // 1. 책등록
     @Transactional(rollbackFor = RuntimeException.class)
-    public BookRespDto 책등록(BookSaveRespDto dto){
+    public BookRespDto 책등록하기(BookSaveRespDto dto){
         Book bookPS = bookRepository.save(dto.toEntity());
         return new BookRespDto().toDto(bookPS);
     }
 
     // 2. 책목록보기
+    public List<BookRespDto> 책목록보기(){
+        return bookRepository.findAll().stream()
+                .map(bookPS -> new BookRespDto().toDto(bookPS))
+                .collect(Collectors.toList());
+    }
 
     // 3. 책한건보기
 
