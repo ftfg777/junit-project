1071eb2b10e323e7c61df26c3ef997b829be0d94
JeongChanWoo <ftfgz5093@gmail.com>
2022-10-03 17:52:33 +0900

    책 등록 테스트 완료

---
+++ src/main/java/com/example/demo/web/dto/BookSaveRespDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.web.dto;
+
+import com.example.demo.domain.Book;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookSaveRespDto {
+    private String title;
+    private String author;
+
+    public Book toEntity(){
+        return Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+    }
+}
+
+
--- src/test/java/com/example/demo/repository/BookRepositoryTest.java
+++ src/test/java/com/example/demo/repository/BookRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.example.demo.repository;
 
+import com.example.demo.domain.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 // DB 관련 테스트
+// 컨트롤러 -> dto -> service -> entity 변환 -> repository -> DB
 @DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 띄움
 public class BookRepositoryTest {
 
@@ -15,7 +19,21 @@ public class BookRepositoryTest {
     // 1. 책 등록
     @Test
     public void 책등록_test(){
-        System.out.println("책등록테스트");
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "정찬우";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        // when (테스트 실행)
+        Book bookPS = bookRepository.save(book);
+
+        // then (검증)
+        assertThat(bookPS.getAuthor()).isEqualTo(author);
+        assertThat(bookPS.getTitle()).isEqualTo(title);
+
     }
 
     // 2. 책 목록보기
