76f351bc9338eeb7eeb38e335eb3200b65476fb1
JeongChanWoo <ftfgz5093@gmail.com>
2022-10-06 01:56:16 +0900

    Sentry log 메세지 추가

--- src/main/java/com/example/demo/config/BindingAdvice.java
+++ src/main/java/com/example/demo/config/BindingAdvice.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 
 import com.example.demo.web.dto.response.CMRespDto;
+import io.sentry.Sentry;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -13,7 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,10 +35,17 @@ public class BindingAdvice {
 
     // 함수제어 : 뒤 After (응답만 제어)
 
-    // Around 통과해야 로그가 남음 우선순위 Around > After > Before
+    // Around 통과해야 로그가 남음 우선순위 Around > Before > After
 
+    // 메소드 진입 전 제어는 필터 사용 / 진입 후 부분제어는 AOP AOP도 전처리가 가능하지만 진입 위치가 다름
+
+
+    // 어떤 함수가 언제 몇 번 실행됐는지 횟수 로그 남기기
     @Before("execution(* com.example.demo.web..*Controller.*(..))")
     public void beforeTest(){
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        log.info("주소 : " + request.getRequestURI());
         log.info("전처리 로그를 남겼습니다");
 
     }
@@ -58,10 +70,16 @@ public class BindingAdvice {
             if(arg instanceof BindingResult){
                 bindingResult = (BindingResult) arg;
             }
+
+            // 서비스 : 정상적인 화면 -> 사용자요청
             if (bindingResult != null && bindingResult.hasErrors()){
                 Map<String, String> errorMap = new HashMap<>();
+
                 for(FieldError fe : bindingResult.getFieldErrors()){
                     errorMap.put(fe.getField(), fe.getDefaultMessage());
+                    // 로그 레벨 error > warn > info > debug
+                    log.warn(type + "." + method + "()=> 필드:"+fe.getField()+", 메시지:"+fe.getDefaultMessage());
+                    Sentry.captureMessage(type + "." + method + "()=> 필드:"+fe.getField()+", 메시지:"+fe.getDefaultMessage());
                 }
                 return new ResponseEntity<>(CMRespDto.builder()
                         .code(-1)
